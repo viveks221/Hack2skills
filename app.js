dd9f200c0d7059f3f6adc4180e44580f8ba1bbde
viveks221 <viveksingh6208@gmail.com>
2024-06-16 13:15:49 +0530

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
-const routes = require("./routes");
+const routes = require("./routes/task");
 
 const app = express();
 const swaggerDocument = YAML.load("./swagger.yml");
@@ -16,7 +16,7 @@ mongoose.connect("mongodb://localhost:27017/task_management", {
 app.use(bodyParser.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use("/tasks", routes);
+app.use("/user", routes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
